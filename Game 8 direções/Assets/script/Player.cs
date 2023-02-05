7535d3de9b490735a539e6b49591fc5fe1284952
Matheus Nogueira <79524016+Theuszin@users.noreply.github.com>
2023-02-04 21:57:03 -0400

    arquivo game jam
    colizor, trocar boneco

--- Game 8 direções/Assets/script/Player.cs
+++ Game 8 direções/Assets/script/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public Rigidbody2D rig;
+    public Rigidbody rig;
 
     public float speed;
 
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
         // Start is called before the first frame update
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
